437cd684327257965557f41c0bb839f0ba41ec62
gubingli <anne.gu@ve-link.com>
2020-09-04 13:56:31 +0800

    JWT

--- project-api/app/Http/Controllers/Admin/HomeController.php
+++ project-api/app/Http/Controllers/Admin/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers\Admin;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 
@@ -9,6 +10,18 @@ class HomeController extends Controller
 {
     public function index()
     {
-        return response_json();
+        $user = User::find(1);
+        $token = auth('users')->fromUser($user);
+        return $this->respondWithToken($token);
+
+    }
+
+    protected function respondWithToken($token)
+    {
+        return response()->json([
+            'access_token' => $token,
+            'token_type' => 'bearer',
+            'expires_in' => auth('users')->factory()->getTTL() * 60
+        ]);
     }
 }
